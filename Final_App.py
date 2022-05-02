b11d36caca48457df8386b80c7b2b5302b77268d
Alex Wei <47674938+AlexWei21@users.noreply.github.com>
2022-05-02 17:44:27 -0400

    1

--- Final_App.py
+++ Final_App.py
@@ -10,28 +10,18 @@ import streamlit as st
 def load_data():
     ## {{ CODE HERE }} ##
     df = pd.read_csv('https://raw.githubusercontent.com/AlexWei21/706_Final_Project/6f129af67cfa5d50a5cb7ced94095d3639e14fda/Covid_19_Full_Data.csv')
+    
     df['Death_per_million'] = df['Deaths'] / df['Population']
     df['Cases_per_million'] = df['Cases'] / df['Population']
-    df['Recovered_per_million'] = df['Recovered'] / df['Population']
+    
     df['Population_Density'] = df['Population'] / df['Area (sq_km)']
-    df['Daily_Death'] = df.groupby(['Country/Region'])['Deaths'].diff()
+
+    df['Daily_Deaths'] = df.groupby(['Country/Region'])['Deaths'].diff()
+    df['Daily_Cases'] = df.groupby(['Country/Region'])['Cases'].diff()
+
     return df
 
 
 df = load_data()
-# df
+df
 
-st.write('## Cases and Death of Covid 19 with Vaccine Data')
-
-### Choosing Time Period
-# date_slider = alt.binding_range(min=min(df['Date']), max=max(df['Date']), step=30)
-
-subset = df[df['Country/Region'] == 'US']
-
-line = alt.Chart(subset).mark_point().encode(
-    x = 'Date:T',
-    y = 'Daily_Death:Q'
-    ).transform_loess('Date','Daily_Death').mark_line()
-    
-
-st.altair_chart(line, use_container_width=True)
