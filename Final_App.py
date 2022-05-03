ccf677918783d88647d8acd971e5cdbfbb20181a
Alex Wei <47674938+AlexWei21@users.noreply.github.com>
2022-05-03 16:53:16 -0400

    1

--- Final_App.py
+++ Final_App.py
@@ -105,49 +105,48 @@ st.altair_chart(W_combine2, use_container_width=True)
 
 st.write("## Covid-19 Information for a selected continent and country")
 
-continent_subset = df
+continent = st.multiselect('Continent',['Asia','European','Africa','North America','South America','Oceania'],['North America'])
 
+subset = subset[subset["Continent"].isin(continent)]
 
-
-C_base = alt.Chart(continent_subset).encode(
+C_base = alt.Chart(subset).encode(
     alt.X('Date:T', axis=alt.Axis(format = '%Y/%m',labelAngle=45))
 )
 
 C_d_area = C_base.mark_area(opacity = 0.5, color = '#FFA500' ).encode(
     # x= alt.X('Date:T', axis=alt.Axis(format = '%Y/%m',labelAngle=45)),
-    alt.Y('Daily_Deaths_y:Q', scale=alt.Scale(domainMin=0)),
+    alt.Y('sum(Daily_Deaths):Q', scale=alt.Scale(domainMin=0)),
     # color= alt.Color("Type"),
-    tooltip=['Date','Daily_Deaths_y']
+    tooltip=['Date','sum(Daily_Deaths)']
 )
 
 C_vaccine_line = C_base.mark_line(color = '#A9A9A9').encode( 
-    y= alt.Y('Global_Vaccination_Rate', axis=alt.Axis(format = '%'), scale=alt.Scale(domain=(0,1))),
+    y= alt.Y('mean(Vaccination_Rate)', axis=alt.Axis(format = '%'), scale=alt.Scale(domain=(0,1))),
     # color= alt.Color("Type"),
-    tooltip=['Date','Global_Vaccination_Rate']
+    tooltip=['Date','mean(Vaccination_Rate)']
 )
 
 C_c_area = C_base.mark_area(opacity = 0.3, color = '#0000FF' ).encode(
     # x= alt.X('Date:T', axis=alt.Axis(format = '%Y/%m',labelAngle=45)),
-    alt.Y('Daily_Cases_y:Q',scale=alt.Scale(domainMin=0)),
+    alt.Y('sum(Daily_Cases):Q',scale=alt.Scale(domainMin=0)),
     # color= alt.Color("Type"),
-    tooltip=['Date','Daily_Cases_y']
+    tooltip=['Date','sum(Daily_Cases)']
 )
 
 C_combine1 = alt.layer(C_d_area,C_vaccine_line).resolve_scale(
     y = 'independent'
 ).properties(
-    title='Global Vaccination Status and Death number'
+    title='Continent Vaccination Status and Death number'
 )
 
 C_combine2 = alt.layer(C_c_area,C_vaccine_line).resolve_scale(
     y = 'independent'
 ).properties(
-    title='Global Vaccination Status and Case number'
+    title='Continent Vaccination Status and Case number'
 )
 
-continent = st.multiselect('Continent',['Asia','European','Africa','North America','South America','Oceania'],['North America'])
-
-subset = subset[subset["Continent"].isin(continent)]
+st.altair_chart(C_combine1, use_container_width=True)
+st.altair_chart(C_combine2, use_container_width=True)
 
 country = st.selectbox('Country', options = subset['Country/Region'].unique())
 
