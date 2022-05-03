f2333174eac570056f674cff92e8aa39cacec54b
Alex Wei <47674938+AlexWei21@users.noreply.github.com>
2022-05-02 21:06:41 -0400

    1

--- Final_App.py
+++ Final_App.py
@@ -97,9 +97,9 @@ vac_data = load_vac_data()
 
 date = st.date_input('Target_Date')
 
-vac_subset = vac_data[(vac_data['Country/Region'] == 'US')]
+vac_subset = vac_data[vac_data['Country/Region'] == 'US']
 
-donut1 = alt.Chart(subset).mark_arc(innerRadius=50, outerRadius=90).encode(
+donut1 = alt.Chart(vac_subset).mark_arc(innerRadius=50, outerRadius=90).encode(
     theta = 'sum(Number):Q',
     color = 'Status',
 )
